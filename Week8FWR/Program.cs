e0fbf1509e983a6222f7d3d47e041e267147eeda
Kaspar P <kaspar.parve@tktk.ee>
2024-12-18 18:16:35 +0200

    WEEK 8

---
+++ Week8FWR/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] tvShows = { "Firefly", "Office", "The Last of Us", "Friends", "Twin Peaks", "House", "Stranger Things" };
+string[] food = { "sushi", "pizza", "Chinese takeaway", "burger and chips", "chicken wings", "chips", "popcorn" };
+string[] drinks = { "mineral water", "Coca-Cola", "apple juice", "milk", "beer" };
+
+string randomMovie = PickRandomFromArry(tvShows);
+string randomFood = PickRandomFromArry(food);
+string randomDrink = PickRandomFromArry(drinks);
+
+Console.WriteLine($"I recommend you to watch {randomMovie} tonight.");
+Console.WriteLine($"Order some {randomFood} and {randomDrink}.");
+
+static string PickRandomFromArry(string[] someArry)
+{
+    Random rnd = new Random();
+
+    int randomIndex = rnd.Next(0, someArry.Length);
+    string randomElement = someArry[randomIndex];
+    return randomElement;
+}
